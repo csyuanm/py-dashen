b26c7c07ba3de30b6dc8cc954c32b59cfcd7f5ce
mml <mml@DESKTOP-KHP5C7D>
2019-04-11 16:38:05 +0800

    完成本章学习

--- learning/lf/chapter6/4-pizza.py
+++ learning/lf/chapter6/4-pizza.py
@@ -1,12 +1,8 @@
 #coding=utf-8
 
-#6.4 嵌套
-#列表中嵌套字典，或字典中嵌套列表，或字典中嵌套字典
-
-
-#6.4.1 字典列表
+#6.4.2 在字典中存储列表
 print('****************')
-print('6.4.1 字典列表')
+print('6.4.2 在字典中存储列表')
 
 #存储所点披萨的信息
 pizza = {
@@ -47,6 +43,52 @@ for name,languages in favorite_languages.items():
             print("\t" + language.title())
     else:
         print(name.title() + "'s favorite language is " + languages[0].title() + ".")
+
+
+#6.4.3 在字典中存储字典
+print('****************')
+print('6.4.3 在字典中存储字典')
+
+users = {
+    'aeinstein': {
+        'first': 'albert',
+        'last': 'aeinstein',
+        'location': 'princeton',
+        },
+    
+    'mcurie': {
+        'first': 'marie',
+        'last': 'curie',
+        'location': 'paris', 
+        },
+    }
+
+for username, user_info in users.items():
+    print("\nUsername: " + username)
+    full_name = user_info['first'] + " " + user_info['last']
+    location = user_info['location']
+    
+    print("\tFull name: " + full_name.title())
+    print("\tlocation: " + location.title())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- learning/lf/chapter6/practice-ch6.py
+++ learning/lf/chapter6/practice-ch6.py
@@ -62,3 +62,109 @@ for name in favorite_languages.keys():
         print(name.title() + ', please jion our investigation.')
 
 
+#6.4 嵌套
+print('****************')
+print('6.4 嵌套 ')
+
+#6-7 人
+name_1 = {'first_name':'Nancy', 'last_name':'Smith', 'age':26, 'city':'Wuhan'}
+name_2 = {'first_name':'Luis', 'last_name':'Merilan', 'age':24, 'city':'Beijing'}
+name_3 = {'first_name':'Jack', 'last_name':'Tomson', 'age':25, 'city':'Nanjing'}
+
+people = [name_1, name_2, name_3]
+
+for name in people:
+    print(name)
+
+#6-8 宠物
+print('6-8 宠物 ')
+
+Mimi= {'type': 'cat', 'owner': 'Eric'}
+Wangwang = {'type': 'dog', 'owner': 'Amanda'}
+Gilly = {'type': 'bird', 'owner': 'Nana'}
+
+pets = [Mimi, Wangwang, Gilly]
+
+for pet in pets:
+    print("It's a " + pet['type'] + " of " + pet['owner'] + ".")
+
+
+#6-9 喜欢的地方
+print('6-9 喜欢的地方 ')
+
+favorite_places = {
+        'Frank': ['China', 'Japan'],
+        'Susan':['Japan'], 
+        'Eric': ['Finland','New Zealand'],
+                       }
+
+for name, places in favorite_places.items():
+    if len(places) > 1:
+        print("\n" + name + "'s favorite places are:")
+        
+        for place in places:  
+            print("\t" + place)
+    
+    else:
+        print("\n" + name + "'s favorite places is " + places[0] + ".")
+        
+
+#6-10 喜欢的数字
+print('6-10 喜欢的数字')
+
+favorite_numbers = {
+    'Lucy':[5,7,9],
+    'Smith':[4,8,9],
+    'Eric':[0,1,5],
+    'Mona':[2,8],
+    'Lily':[6,8],
+    }
+
+
+for name, numbers in favorite_numbers.items():
+    print("\n" + name + "'s favorite numbers are:")
+                
+    for number in numbers:  
+        print("\t" + str(number))
+
+#6-11 城市
+print('6-11 城市')
+
+cities = {
+    'Guangzhou':{
+        'country':'China',
+        'population': 17,
+        'fact': 'It locates in South of China.'
+        },
+    
+    'Tokyo':{
+        'country':'Japan',
+        'population': 20,
+        'fact': 'It is the national capital of Japan.'
+    },
+    
+    'Los Angeles':{
+        'country':'America',
+        'population': 15,
+        'fact': 'It is a  prosperous city.'
+    },
+    
+    }
+
+for city, information in cities.items():
+    print("\n" + city + "'s information:")
+                    
+    print("Country: " + information['country'] + ".")
+    print("Population: " + str(information['population']) + " million.")
+    print("Fact: " + information['country'])
+
+
+
+
+
+
+
+
+
+
+
