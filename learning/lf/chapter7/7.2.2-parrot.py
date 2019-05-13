facc0219f7ace6dc0490f734f6482fc4af7f2c2d
Administrator <Administrator@PC-20180208XFTW>
2019-05-13 23:43:05 +0800

    1

--- learning/lf/chapter7/7.2.2-parrot.py
+++ learning/lf/chapter7/7.2.2-parrot.py
@@ -2,15 +2,16 @@
 
 #7.2.2 让用户选择何时退出
 
+#1)单词quit也打印出来
 prompt = "\nTell me something, and I will repeat it back to you:"
 prompt += "\nEnter 'quit' to end the program."
 
 message = ""      #初始值为一个空白字符串，让python能够执行while循环所需的比较
 # while message != 'quit':
-#     message = input(prompt)  #只要message的值不是'quit'，这个循环就会不断运行
+#     message = input(prompt)  #只要message的值不是'quit'，这个循环就会不断运行                                                                                          
 #     print(message)
 
-
+#2)使用一个if测试，使得单词quit不打印出来
 prompt = "\nTell me something, and I will repeat it back to you:"
 prompt += "\nEnter 'quit' to end the program."
 
@@ -25,6 +26,26 @@ while message != 'quit':
         message = 'quit'
 
 
+
+#7.2.3 使用标志
+print('****************')
+print('7.2.3 使用标志')
+
+#在需要很多条件都满足才能继续运行的程序中，可定义一个变量（即标志），用于判断程序是否处于活动状态
+prompt = "\nTell me something, and I will repeat it back to you:"
+prompt += "\nEnter 'quit' to end the program."
+
+active = True
+while active:
+    message = input(prompt)
+    
+    if message == 'quit':
+        active = False
+    elif message == 'exit':
+        active = False
+    else:
+        print(message)    
+        
 
 
 
---
+++ learning/lf/chapter7/7.2.4-cities.py
@@ -0,0 +1,30 @@
+#coding=utf-8
+
+
+#7.2.4 使用break退出循环
+print('****************')
+print('7.2.4 使用break退出循环')
+
+prompt = "Please input your ID:"
+
+
+flag=True
+name = ''
+while flag:
+    name = input(prompt)
+    
+    m=int(input(prompt+"2=mmmmmmmmmmmmmm"))
+    n=int(input(prompt+"nnnnnnnnnnnnnnnnnnS"))
+    l=m+n
+    print(name)
+    print(l)
+    
+    if name == str(l):
+        flag = False
+    
+    
+    
+    
+    
+    
+
